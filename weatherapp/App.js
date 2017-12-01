0551b11a73950b36ec4ae428e33ed59d8b431580
yunsupj <yunsup.j@gmail.com>
2017-11-30 22:17:30 -0800

    final_ver

--- weatherapp/App.js
+++ weatherapp/App.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import Weather from "./Weather";
+import Weather from './Weather';
+
+const API_Key = "16f3c0560f0938280ba5681ca0e82feb";
 
 export default class App extends Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null,
+    temp: null,
+    name: null,
+    city: null
   };
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: "*** Couldn't get Weather Informations ***"
+        });
+      }
+    );
+  }
+
+  _getWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_Key}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temp: json.main.temp,
+          name: json.weather[0].main,
+          city: json.name
+        });
+      })
+
+      .then(setTimeout(() => {
+        this.setState({
+          isLoaded: true,
+        });
+      }, 4000))
+  }
+
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error, name, temp, city } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
-        { isLoaded ? (
-            <Weather />
-          ) : (
+        {isLoaded ? (
+          <Weather
+            weatherName={name}
+            temp={Math.floor(temp-273.15)}
+          />
+        ) : (
           <View style={styles.loading}>
-            <Text style={styles.loadingText}>Hi Kim, Chloe. Todays Weather  @Emeryville</Text>
+            <StatusBar barStyle="dark-content" />
+            <Text style={styles.loadingText}>Hi Kim, Chloe. Todays Weather   @{city}</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text>: null}
           </View>
         )}
       </View>
@@ -26,18 +70,28 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+
+  errorText: {
+    fontSize: 30,
+    color: "red",
+    backgroundColor: "transparent",
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginTop: 200
   },
 
   loading: {
     flex: 1,
     backgroundColor: "#FDF6AA",
-    justifyContent: "flex-end",
-    paddingLeft: 25
+    justifyContent: "flex-start",
+    paddingLeft: 30
   },
 
   loadingText: {
     fontSize: 38,
-    marginBottom: 100
+    marginTop: 120
   }
 });
--- weatherapp/Weather.js
+++ weatherapp/Weather.js
@@ -1,24 +1,100 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class Weather extends Component {
-  render() {
-    return (
-      <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
-        <View style={styles.upper}>
-          <Ionicons name="ios-rainy" size={144} color="white" />
-          <Text style={styles.temp}>Temp Here!</Text>
-        </View>
-        <View style={styles.lower}>
-          <Text style={styles.title}>Heavy Rain</Text>
-          <Text style={styles.subtitle}>For more info look outside</Text>
-        </View>
-      </LinearGradient>
-    );
+const weatherCases = {
+  Rain: {
+    colors: ["#00C6FB", "#005BEA"],
+    title: "Rainy",
+    subtitle: "Bring an Umbrella!!",
+    icon: "weather-rainy"
+  },
+
+  Clear: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Sunny",
+    subtitle: "Go get your ass Burn!!",
+    icon: "weather-sunny"
+  },
+
+  Thunderstorm: {
+    colors: ["#00ECBC", "#007ADF"],
+    title: "Thunderstorm",
+    subtitle: "Stay in your House!!",
+    icon: "weather-lightning"
+  },
+
+  Clouds: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Clouds",
+    subtitle: "Gloomy Day!!",
+    icon: "weather-cloudy"
+  },
+
+  Snow: {
+    colors: ["#7DE2FC", "#B9B6E5"],
+    title: "Snow",
+    subtitle: "Go out with your Wife, and Enjoy it!!",
+    icon: "weather-snowy"
+  },
+
+  Drizzle: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Drizzle",
+    subtitle: "You can see 🌈. May be after shallow Rain",
+    icon: "weather-hail"
+  },
+
+  Haze: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Haze",
+    subtitle: "Again? Wait for Sunny Day!!",
+    icon: "weather-hail"
+  },
+
+  Mist: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on.",
+    icon: "weather-fog"
   }
+};
+
+function Weather({weatherName, temp}) {
+  return (
+    <LinearGradient
+      colors={weatherCases[weatherName].colors}
+      style={styles.container}
+    >
+      <View style={styles.upper}>
+        <MaterialCommunityIcons
+          name={weatherCases[weatherName].icon}
+          size={144}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}º</Text>
+      </View>
+
+      <View style={styles.lower}>
+        <Text style={styles.title}>
+          {weatherCases[weatherName].title}
+        </Text>
+        <Text style={styles.subtitle}>
+          {weatherCases[weatherName].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
 }
+
+Weather.propTypes = {
+  weatherName: PropTypes.string.isRequired,
+  temp: PropTypes.number.isRequired
+};
+
+export default Weather;
 
 const styles = StyleSheet.create({
   container: {
@@ -33,7 +109,7 @@ const styles = StyleSheet.create({
   },
 
   temp: {
-    fontSize: 48,
+    fontSize: 70,
     backgroundColor: "transparent",
     color: "white",
     marginTop: 10
@@ -43,23 +119,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "flex-start",
     justifyContent: "flex-end",
-    paddingLeft: 25
+    paddingLeft: 25,
+    paddingRight: 25
   },
 
   title: {
     fontSize: 38,
     backgroundColor: "transparent",
     color: "white",
-    marginBottom: 10
+    marginBottom: 10,
+    fontWeight: "300"
   },
 
   subtitle: {
     fontSize: 24,
     backgroundColor: "transparent",
     color: "white",
-    marginBottom: 24
+    marginBottom: 50
   }
-
-
-
 });
